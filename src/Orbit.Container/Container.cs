119bb930c2bcb82a365de616fc878514daa189e8
Sami Daniel <samidanielpersonal@gmail.com>
2025-06-23 14:46:11 -0300

    feat: implement dependency injection setup for database and identity services in Orbit.Container

---
+++ src/Orbit.Container/Container.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Orbit.Infrastructure.Data.Contexts;
+using Orbit.Infrastructure.Identity;
+
+namespace Orbit.Container;
+
+public static class Container
+{
+    public static void SetupContainer(this IServiceCollection services)
+    {
+        string connectionString;
+        connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
+        ?? throw new InvalidOperationException("The connection String cannot be empty.");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        services.AddIdentityCore<ApplicationUser>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 6;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = true;
+            options.SignIn.RequireConfirmedPhoneNumber = true;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+    }
+}
